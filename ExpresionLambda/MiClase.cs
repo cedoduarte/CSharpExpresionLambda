fcdb47434faa2e9649de83d8b9a4fe39f6e34ece
cedoduarte <carlosduarte.1@hotmail.com>
2015-11-29 20:05:46 -0600

    first commit

---
+++ ExpresionLambda/MiClase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpresionLambda
+{
+    public class MiClase
+    {
+        public void Inicia()
+        {
+            int[] arreglo = { 1, 2, 3, 4, 5 };
+            ParaCadaUno(arreglo, (int ix) => 
+            {
+                Console.WriteLine(ix * 2);
+            });
+            Console.Read();
+        }
+
+        private void ParaCadaUno(int[] arreglo, Funcion funcion)
+        {
+            foreach (int ix in arreglo)
+            {
+                funcion(ix);
+            }
+        }
+
+        private delegate void Funcion(int ix);
+    }
+}
